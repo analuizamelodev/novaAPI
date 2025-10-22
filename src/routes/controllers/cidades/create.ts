b3e72f234d355d85aa4ec514d41b8d0dff0ba2f9
analuizamelodev <analuizamelodasvirgens@gmail.com>
2025-10-22 17:15:35 -0300

    feat: update cidade creation schema and improve error handling

--- src/routes/controllers/cidades/create.ts
+++ src/routes/controllers/cidades/create.ts
@@ -2,24 +2,30 @@ import { Request, Response } from "express";
 import { z } from "zod";
 
 interface Cidade {
-  nome: string;
+  cidade: string;
+  estado: string;
 }
 
 const cidadeSchema = z.object({
-  nome: z.string().min(3).max(50),
+  cidade: z.string().min(3, "A Cidade deve ter pelo menos 3 caracteres.").max(50, "A cidade deve ter no máximo 50 caracteres."),
+  estado: z.string().length(2, "O Estado deve ter exatamente 2 letras."),
 });
 
 export const create = (req: Request<{}, {}, Cidade>, res: Response) => {
   const result = cidadeSchema.safeParse(req.body);
 
   if (!result.success) {
-    const mensagens = result.error.issues.map((issue) => issue.message);
+    const mensagens = result.error.issues.map((issue) => {
+      const campo = issue.path.join(".");
+      return `Erro em '${campo}': ${issue.message}`;
+    });
 
     return res.status(400).json({
       message: mensagens.length === 1 ? mensagens[0] : mensagens,
     });
   }
 
-  console.log(req.body);
-  return res.send("Cidade criada com sucesso!");
+  return res.status(201).send({
+    message: "Cidade e Estado criados com sucesso!",
+  });
 };
